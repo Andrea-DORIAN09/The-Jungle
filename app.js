0ad8ea58918cf64591517e48ce6b301cf7515f35
Andrea Dorian Perez Cadenas <andreadorian.pc@gmail.com>
2016-11-17 15:13:22 -0600

    Update The Connection String

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ var newp=require('./routes/newp');
 var app = express();
 
 // Connect to database
-mongoose.connect('mongodb://localhost/products');
-mongoose.connect('process.env.MONGODB_URI');
+//mongoose.connect('mongodb://localhost/products');
+mongoose.connect(process.env.MONGODB_URI);
 
 var db = mongoose.connection;
 
